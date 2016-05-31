2874ab944b6b8dc181f6da8e101a83f4c61fe07b
zhulei <691698513@qq.com>
2016-05-31 23:34:55 +0800

    外面播放器可控制播放

--- HaoChang/PlayControlViewController.m
+++ HaoChang/PlayControlViewController.m
@@ -8,10 +8,14 @@
 
 #import "PlayControlViewController.h"
 #import "UIImage+RoundedCorner.h"
+#import "MacroDefinition.h"
+#import "HCPlayer.h"
 
 @interface PlayControlViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *coverImageView;
-
+@property (weak, nonatomic) IBOutlet UIButton *playBtn;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (strong, nonatomic) HCPlayer *HCPlayer;
 @end
 
 @implementation PlayControlViewController
@@ -19,15 +23,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    _HCPlayer = [HCPlayer sharedInstance];
     UIImage *coverImage = [UIImage imageNamed:@"albumArt5"];
     CGSize imageSize = coverImage.size;
     self.coverImageView.image = [coverImage roundedCornerImageWithCornerRadius:imageSize.width / 2];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    [self.slider setValue:0];
+    
+    UITapGestureRecognizer *tapGesture=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(showPlayer)];
+    [self.view addGestureRecognizer:tapGesture];
+    [self.HCPlayer addObserver:self forKeyPath:@"duration" options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew context:nil];
+    [self.HCPlayer addObserver:self forKeyPath:@"currentTime" options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew context:nil];
 }
 
 - (BOOL)prefersStatusBarHidden
@@ -35,6 +41,37 @@
     return NO;
 }
 
+- (void)showPlayer
+{
+    [[NSNotificationCenter defaultCenter] postNotificationName:kNotificationNamePlaySong object:nil];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context
+{
+    if ([keyPath isEqualToString:@"duration"]) {
+        [self.slider setMaximumValue:self.HCPlayer.duration];
+    } else if ([keyPath isEqualToString:@"currentTime"]) {
+        [self.slider setValue:self.HCPlayer.currentTime animated:YES];
+    }
+}
 
 
+- (IBAction)play:(id)sender
+{
+    if (self.HCPlayer.isPlaying) {
+        [self.HCPlayer pause];
+        [self.playBtn setImage:[UIImage imageNamed:@"player_play"] forState:UIControlStateNormal];
+    } else {
+        if (![self.HCPlayer currentSongURL]) {
+            NSURL *songURL = [NSURL URLWithString:@"http://sc1.111ttt.com/2016/5/02/25/195251254501.mp3"];
+            [self.HCPlayer setSongURL:songURL];
+        }
+        [self.HCPlayer play];
+        [self.playBtn setImage:[UIImage imageNamed:@"player_pause"] forState:UIControlStateNormal];
+    }
+}
+
+- (IBAction)list:(id)sender
+{
+}
 @end
